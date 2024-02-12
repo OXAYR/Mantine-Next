540d31f7416934404ba28d43de0949516715127e
OXAYR <130404981+OXAYR@users.noreply.github.com>
2024-02-12 17:32:43 +0500

    Added gradient props

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import { Title, Box } from "@mantine/core";
+import { Title, Box, Text } from "@mantine/core";
 
 const Index = () => {
 	return (
@@ -6,12 +6,21 @@ const Index = () => {
 			{/* by passing props to the title of component we have made it polymorphic */}
 
 			<Title
-				order={3}
-				component="p"
+				order={1}
+				component="h1"
 				align="center"
+				variant="gradient"
+				gradient={{ from: "blue", to: "pink", deg: 90 }}
 			>
 				Hello from the title
 			</Title>
+
+			<Text
+				variant="gradient"
+				gradient={{ from: "blue", to: "pink", deg: 90 }}
+			>
+				Hello it is a texxt component
+			</Text>
 		</Box>
 	);
 };
